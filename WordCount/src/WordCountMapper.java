54add98d70b9f95700931b5a48aa4ca25188dece
cloudera <cloudera@quickstart.cloudera>
2018-07-13 23:38:50 -0700

    Word Count - Completion

--- WordCount/src/WordCountMapper.java
+++ WordCount/src/WordCountMapper.java
@@ -1,4 +1,20 @@
+import java.io.IOException;
 
-public class WordCountMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 
+public class WordCountMapper extends
+		Mapper<LongWritable, Text, Text, IntWritable> {
+
+	@Override
+	protected void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		String input = value.toString();
+		String[] strings = input.split(" ");
+		for (String s : strings) {
+			context.write(new Text(s), new IntWritable(1));
+		}
+	}
 }
--- WordCount/src/WordCountReducer.java
+++ WordCount/src/WordCountReducer.java
@@ -1,4 +1,19 @@
+import java.io.IOException;
 
-public class WordCountReducer {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
 
+public class WordCountReducer extends
+		Reducer<Text, IntWritable, Text, IntWritable> {
+
+	@Override
+	protected void reduce(Text key, Iterable<IntWritable> value, Context context)
+			throws IOException, InterruptedException {
+		int count = 0;
+		for (IntWritable i : value) {
+			count += i.get();
+		}
+		context.write(key, new IntWritable(count));
+	}
 }
